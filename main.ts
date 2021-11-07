fce181fd9f3276106ce94d434f13dd960da0ffe9
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2021-11-07 19:18:53 +0100

    .

--- main.ts
+++ main.ts
@@ -1,10 +1,9 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
-import deleteMe from "./integrations/delete-me.ts";
 
 const app = new Application();
 
 app.use((ctx: any) => {
-  ctx.response.body = deleteMe();
+  ctx.response.body = "hi";
 });
 
 await app.listen({ port: 7600 });
