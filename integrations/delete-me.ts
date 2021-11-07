8c98fe65e209d2385d38dbbd38387f9f73e24e49
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2021-11-07 18:59:52 +0100

    testing sub-functions

---
+++ integrations/delete-me.ts
@@ -0,0 +1,3 @@
+export default function deleteMe() {
+  return "Delete me";
+}
--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
+import deleteMe from "./integrations/delete-me.ts";
 
 const app = new Application();
 
 app.use((ctx: any) => {
-  ctx.response.body = "Hello World!";
+  ctx.response.body = deleteMe();
 });
 
 await app.listen({ port: 7600 });
